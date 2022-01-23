78f682853b907ba34309056b69d4a7945096292c
Harry <36096251+harry-sunhao@users.noreply.github.com>
2022-01-24 00:43:46 +0800

    Update symbolab.js

--- Symbolab/symbolab.js
+++ Symbolab/symbolab.js
@@ -5,7 +5,7 @@ Symbolab 解锁高级功能 (需登录)
 QuantumultX:
 
 [rewrite_local]
-^https?:\/\/scibug\.com\/appleSubscriptionValidate url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/VUE.js
+^https?:\/\/scibug\.com\/appleSubscriptionValidate url script-response-body https://raw.githubusercontent.com/harry-sunhao/QuanX/main/Symbolab/symbolab.js
 
 [mitm]
 hostname = scibug.com
