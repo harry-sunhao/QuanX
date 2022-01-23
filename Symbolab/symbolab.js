e43d8419637fba58cab546424854aa7f5df2a74e
Harry <36096251+harry-sunhao@users.noreply.github.com>
2022-01-24 00:59:54 +0800

    Update symbolab.js

--- Symbolab/symbolab.js
+++ Symbolab/symbolab.js
@@ -5,9 +5,9 @@ Symbolab 解锁高级功能 (需登录)
 let obj = JSON.parse($response.body);
 
 obj.membership = {
-    "valid": true,
-    "newlyAssociated": false,
-    "hasUserConsumedAppleFreeTrial": false
-  }
+    "valid":true,
+    "hasUserConsumedAppleFreeTrial":false,
+    "newlyAssociated":false
+    }
 
 $done({body: JSON.stringify(obj)});
