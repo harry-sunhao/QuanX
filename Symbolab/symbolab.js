bf35d50e67f272ac8fb8f00d7bbb6258b4afbc04
Harry <36096251+harry-sunhao@users.noreply.github.com>
2022-03-13 02:32:44 +0800

    Update symbolab.js

--- Symbolab/symbolab.js
+++ Symbolab/symbolab.js
@@ -1,5 +1,26 @@
 /*
-Symbolab 解锁高级功能 (需登录)
+脚本功能：Symbolab 解锁高级功能 (需登录)
+软件版本：3.8.1
+下载地址：https://apps.apple.com/cn/app/symbolab%E8%AE%A1%E7%AE%97%E5%99%A8/id876942533
+脚本作者：吾爱破解论坛 云在天
+更新时间：2022.03.13
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+
+********************************
+
+[rewrite_local]
+
+# Symbolab 解锁订阅
+^https?:\/\/scibug\.com\/appleSubscriptionValidate$ url script-response-body https://raw.githubusercontent.com/harry-sunhao/QuanX/main/Symbolab/symbolab.js
+
+[mitm] 
+
+hostname = scibug.com
+*
+*
+*/
+
 
 ***************************
 let obj = JSON.parse($response.body);
