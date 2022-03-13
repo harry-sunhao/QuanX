0d678d4082798339e5a81008c8b08442ffbdad74
Harry <36096251+harry-sunhao@users.noreply.github.com>
2022-03-13 23:11:59 +0800

    Update smqnw.js

--- smqnw.js
+++ smqnw.js
@@ -12,27 +12,9 @@
 Quantumult X:
 
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/harry-sunhao/QuanX/main/smqnw.js
 
 [mitm]
-hostname = ap*.intsig.net
-
-***************************
-Surge4 or Loon:
-
-[Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-
-[MITM]
-hostname = ap*.intsig.net
-
-***************************
-Quantumult:  
-
-[REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
-
-[MITM]
 hostname = ap*.intsig.net
 
 **************************/
