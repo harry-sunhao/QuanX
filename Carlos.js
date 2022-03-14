b4fac4beaead4d34ebe351424d841155148c7072
Harry <36096251+harry-sunhao@users.noreply.github.com>
2022-03-14 08:05:52 +0800

    Update Carlos.js

--- Carlos.js
+++ Carlos.js
@@ -1,2 +1,2 @@
-var body = $response.body .replace(/\"isBuy\":false/g,"\"isBuy\":true");
+var body = $response.body .replace(/\"isBuy\":false/g,"\"isBuy\":true").replace(/\"chargeType\":4/g,"\"chargeType\":1");
 $done({ body });
